1613c17817f8c0a202e952ce49ae8fd253035806
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-21 09:23:40 -0800

    Updated

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+//navigation
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import { NavigationContainer } from '@react-navigation/native'
+const App = () => {
+  return (
+    <View>
+      <Text>Mobile app with navigation</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
